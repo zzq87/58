04d302a31b461b5c27a3d0718ec3cd1fc64b2dd4
zzqandy <zzqandy@163.com>
2016-12-03 11:52:44 +0800

    testip.py验证数据库内IP

--- testip.py
+++ testip.py
@@ -4,6 +4,7 @@ import requests
 con = sqlite3.connect('./ip.db')
 cur = con.cursor()
 ip = cur.execute("select ip from ip")
+uip = []
 for i in ip:
     #print(i[0])
     proxies = {"http":i[0]}
@@ -14,4 +15,15 @@ for i in ip:
         else:
             print("无效", i)
     except:
-        print("失败", i)
+        print('失败', i)
+        uip.append(i[0])
+
+for i in uip:
+    cur.execute("delete from ip where ip = '{}'".format(i))
+
+con.commit()
+nip = cur.execute("select ip from ip")
+for i in nip:
+    print(i[0])
+cur.close()
+con.close()
