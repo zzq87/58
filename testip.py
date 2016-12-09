8775abc7da4c16e06cdbad9eda17ba8a21419845
zzqandy <zzqandy@163.com>
2016-12-09 09:35:16 +0800

    改为线程验证IP可用性

--- testip.py
+++ testip.py
@@ -1,27 +1,45 @@
-import sqlite3
+import sqlite3,time
 import requests
+import queue
+from threading import Thread
 
 con = sqlite3.connect('./ip.db')
 cur = con.cursor()
 ip = cur.execute("select ip from ip")
-uip = []
+uip = queue.Queue()
+q = queue.Queue()
 for i in ip:
-    #print(i[0])
-    proxies = {"http":i[0]}
-    try:
-        t = requests.get("http://www.baidu.com", proxies=proxies, timeout=5)
-        if t.status_code == 200:
-            print("可用",i)
+    q.put(i[0])
+def testip(q, uip):
+    while True:
+        if q.empty():
+            break
         else:
-            print("无效", i)
-    except:
-        print('失败', i)
-        uip.append(i[0])
-
-for i in uip:
-    cur.execute("delete from ip where ip = '{}'".format(i))
-
+            ip = q.get()
+            proxies = {"http":ip}
+            try:
+                t = requests.get("http://ip.chinaz.com/", proxies=proxies, timeout=5)
+                if t.status_code == 200:
+                    print("可用",ip)
+                else:
+                    print("无效", ip)
+            except:
+                print('失败', ip)
+                uip.put(ip)
+    return uip
+for i in range(5):
+    t = Thread(target=testip, args=(q,uip))
+    t.start()
+t.join()
+time.sleep(3)
+def duip(uip):
+    while True:
+        if uip.empty():
+            break
+        else:
+            cur.execute("delete from ip where ip = '{}'".format(uip.get()))
 con.commit()
+
 nip = cur.execute("select ip from ip")
 for i in nip:
     print(i[0])
