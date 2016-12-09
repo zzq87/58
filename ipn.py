c8768cd8fd25e5717f467f3905206054f0a005e1
zzqandy <zzqandy@163.com>
2016-12-09 21:52:43 +0800

    ipn

--- ipn.py
+++ ipn.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 import requests
+from bs4 import BeautifulSoup
 pro = {"http":"124.88.67.20:80"}
 s = requests.get("http://ip.chinaz.com", proxies=pro).text
-print(s)
+soup  = BeautifulSoup(s, "lxml")
+ip = soup.find_all(class_="fz24")
+for i in ip:
+    print(i)
