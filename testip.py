e598912bf8658fb218f3ad51b3600f9329cfe174
zzqandy <zzqandy@163.com>
2016-12-09 16:16:53 +0800

    b-s

--- testip.py
+++ testip.py
@@ -5,6 +5,7 @@ from threading import Thread
 
 con = sqlite3.connect('./ip.db')
 cur = con.cursor()
+#cur.execute("create table ipn (id integer primary key autoincrement,ip char)")
 ip = cur.execute("select ip from ip")
 uip = queue.Queue()
 q = queue.Queue()
@@ -31,17 +32,20 @@ for i in range(5):
     t = Thread(target=testip, args=(q,uip))
     t.start()
 t.join()
-time.sleep(3)
+time.sleep(1)
 def duip(uip):
     while True:
         if uip.empty():
             break
         else:
             cur.execute("delete from ip where ip = '{}'".format(uip.get()))
-con.commit()
-
-nip = cur.execute("select ip from ip")
+    con.commit()
+duip(uip)
+nip = cur.execute("select distinct ip from ip").fetchall()
 for i in nip:
-    print(i[0])
+    proip = i[0]
+    print('......',proip)
+    cur.execute("insert into ipn(ip)values('{}')".format(proip))
+con.commit()
 cur.close()
 con.close()
